6712f05051ed0eea0aac616fd4be0d37f1b41605
Luiz Parreira <luiz@magnetis.com.br>
2019-08-15 18:13:46 -0300

    Add Docker and install dependencies

--- config/dev.exs
+++ config/dev.exs
@@ -3,9 +3,9 @@ use Mix.Config
 # Configure your database
 config :event_validator, EventValidator.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "event_validator_dev",
-  hostname: "localhost",
+  hostname: "db",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
