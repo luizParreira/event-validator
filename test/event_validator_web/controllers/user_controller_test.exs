71df9be47d9d833f3e02e66edbc962552d86ff1e
Bruno Gomes <brunojabs@gmail.com>
2019-08-25 23:21:51 -0300

    Remove uses of conn/0 that's deprecated from users controller test
    These were causing warning during test run

--- test/event_validator_web/controllers/user_controller_test.exs
+++ test/event_validator_web/controllers/user_controller_test.exs
@@ -22,7 +22,7 @@ defmodule EventValidatorWeb.UserControllerTest do
 
   describe "create user" do
     test "renders user when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.user_path(conn, :create), user: @create_attrs)
+      conn = post(build_conn(), Routes.user_path(conn, :create), user: @create_attrs)
       response = json_response(conn, 201)["data"]
 
       assert %{"id" => id} = response
@@ -30,12 +30,12 @@ defmodule EventValidatorWeb.UserControllerTest do
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.user_path(conn, :create), user: @invalid_attrs)
+      conn = post(build_conn(), Routes.user_path(conn, :create), user: @invalid_attrs)
       assert json_response(conn, 422)["errors"] != %{}
     end
 
-    test "renders bad request when the payload is incorrect" do
-      conn = post(conn, Routes.user_path(conn, :create), other: %{invalid: "payload"})
+    test "renders bad request when the payload is incorrect", %{conn: conn} do
+      conn = post(build_conn(), Routes.user_path(conn, :create), other: %{invalid: "payload"})
 
       assert json_response(conn, 400)["errors"] == %{"title" => "BadRequest"}
     end
