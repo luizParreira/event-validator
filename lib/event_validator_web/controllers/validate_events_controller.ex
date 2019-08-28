3be50417dafb38c2c2b994af94b5333b6464f333
Luiz Parreira <luiz@magnetis.com.br>
2019-08-28 16:14:58 -0300

    Create pipeline to authenticate validation endpoint
    - Created plug that will be used to validate event validation endpoint.
    - Created sample controller to test event validation authentication
    pipeline

---
+++ lib/event_validator_web/controllers/validate_events_controller.ex
@@ -0,0 +1,11 @@
+defmodule EventValidatorWeb.ValidateEventsController do
+  use EventValidatorWeb, :controller
+
+  action_fallback EventValidatorWeb.FallbackController
+
+  def create(conn, params) do
+    conn
+    |> put_status(200)
+    |> render("validate_events.json", source_id: conn.assigns.source_id)
+  end
+end
