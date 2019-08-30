eb356940ee3b5e0eea8d856ac02d036dca54dc7d
Luiz Parreira <luiz@magnetis.com.br>
2019-08-30 08:29:58 -0300

    Fixed unused warnings

--- lib/event_validator/events/event_schema.ex
+++ lib/event_validator/events/event_schema.ex
@@ -2,9 +2,12 @@ defmodule EventValidator.Events.EventSchema do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias EventValidator.Projects.Source
+
   schema "event_schemas" do
     field :name, :string
     field :schema, :map
+    belongs_to :source, Source, foreign_key: :source_id
 
     timestamps()
   end
@@ -12,7 +15,8 @@ defmodule EventValidator.Events.EventSchema do
   @doc false
   def changeset(event_schema, attrs) do
     event_schema
-    |> cast(attrs, [:name, :schema])
-    |> validate_required([:name, :schema])
+    |> cast(attrs, [:name, :schema, :source_id])
+    |> validate_required([:name, :schema, :source_id])
+    |> foreign_key_constraint(:source_id)
   end
 end
--- lib/event_validator_web/controllers/validate_events_controller.ex
+++ lib/event_validator_web/controllers/validate_events_controller.ex
@@ -3,7 +3,7 @@ defmodule EventValidatorWeb.ValidateEventsController do
 
   action_fallback EventValidatorWeb.FallbackController
 
-  def create(conn, params) do
+  def create(conn, _params) do
     conn
     |> put_status(200)
     |> render("validate_events.json", source_id: conn.assigns.source_id)
