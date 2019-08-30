9c54845a4baced7d0157d42975f2f03982d1f63a
Luiz Parreira <luiz@magnetis.com.br>
2019-08-30 08:30:10 -0300

    Add `has_one` relationship to `source`
    - Create migration
    - Adapt tests

--- test/event_validator/events_test.exs
+++ test/event_validator/events_test.exs
@@ -1,19 +1,47 @@
 defmodule EventValidator.EventsTest do
   use EventValidator.DataCase
 
-  alias EventValidator.Events
+  alias EventValidator.{Events, Accounts, Projects}
 
   describe "event_schemas" do
     alias EventValidator.Events.EventSchema
 
-    @valid_attrs %{name: "some name", schema: %{}}
+    @event_schema_attrs %{name: "some name", schema: %{}, source_id: nil}
     @update_attrs %{name: "some updated name", schema: %{}}
-    @invalid_attrs %{name: nil, schema: nil}
+    @invalid_attrs %{name: nil, schema: nil, source_id: nil}
+
+    @user_attrs %{
+      email: "some@email.com",
+      password: "some password",
+      name: "some name"
+    }
+
+    @source_attrs %{
+      name: "some name",
+      organization_id: nil
+    }
+
+    @org_attrs %{
+      name: "some name",
+      size: "1-10",
+      website: "some website"
+    }
+
+    def setup_fixture do
+      with {:ok, user} <- Accounts.create_user(@user_attrs),
+           {:ok, organization} <- Accounts.create_organization(@org_attrs, user.id),
+           {:ok, source} <-
+             Projects.create_source(%{@source_attrs | organization_id: organization.id}) do
+        source
+      end
+    end
 
     def event_schema_fixture(attrs \\ %{}) do
+      source = setup_fixture()
+
       {:ok, event_schema} =
         attrs
-        |> Enum.into(@valid_attrs)
+        |> Enum.into(%{@event_schema_attrs | source_id: source.id})
         |> Events.create_event_schema()
 
       event_schema
@@ -30,7 +58,8 @@ defmodule EventValidator.EventsTest do
     end
 
     test "create_event_schema/1 with valid data creates a event_schema" do
-      assert {:ok, %EventSchema{} = event_schema} = Events.create_event_schema(@valid_attrs)
+      assert {:ok, %EventSchema{} = event_schema} = {:ok, event_schema_fixture()}
+
       assert event_schema.name == "some name"
       assert event_schema.schema == %{}
     end
@@ -41,14 +70,20 @@ defmodule EventValidator.EventsTest do
 
     test "update_event_schema/2 with valid data updates the event_schema" do
       event_schema = event_schema_fixture()
-      assert {:ok, %EventSchema{} = event_schema} = Events.update_event_schema(event_schema, @update_attrs)
+
+      assert {:ok, %EventSchema{} = event_schema} =
+               Events.update_event_schema(event_schema, @update_attrs)
+
       assert event_schema.name == "some updated name"
       assert event_schema.schema == %{}
     end
 
     test "update_event_schema/2 with invalid data returns error changeset" do
       event_schema = event_schema_fixture()
-      assert {:error, %Ecto.Changeset{}} = Events.update_event_schema(event_schema, @invalid_attrs)
+
+      assert {:error, %Ecto.Changeset{}} =
+               Events.update_event_schema(event_schema, @invalid_attrs)
+
       assert event_schema == Events.get_event_schema!(event_schema.id)
     end
 
--- test/event_validator_web/controllers/event_schema_controller_test.exs
+++ test/event_validator_web/controllers/event_schema_controller_test.exs
@@ -1,9 +1,8 @@
 defmodule EventValidatorWeb.EventSchemaControllerTest do
   use EventValidatorWeb.ConnCase
 
-  alias EventValidator.Events
-  alias EventValidator.Events.EventSchema
-  alias EventValidator.JWT
+  alias EventValidator.{Events, Accounts, Projects, JWT}
+  alias Events.EventSchema
 
   @user_attrs %{
     email: "some@email.com",
@@ -13,28 +12,50 @@ defmodule EventValidatorWeb.EventSchemaControllerTest do
 
   @create_attrs %{
     name: "some name",
-    schema: %{}
+    schema: %{},
+    source_id: nil
   }
+
+  @source_attrs %{
+    name: "some name",
+    organization_id: nil
+  }
+
+  @org_attrs %{
+    name: "some name",
+    size: "1-10",
+    website: "some website"
+  }
+
   @update_attrs %{
     name: "some updated name",
     schema: %{}
   }
-  @invalid_attrs %{name: nil, schema: nil}
 
-  def fixture(:event_schema) do
-    {:ok, event_schema} = Events.create_event_schema(@create_attrs)
+  @invalid_attrs %{name: nil, schema: nil, source_id: nil}
+
+  def setup_fixture do
+    {:ok, user} = Accounts.create_user(@user_attrs)
+    {:ok, organization} = Accounts.create_organization(@org_attrs, user.id)
+    {:ok, source} = Projects.create_source(%{@source_attrs | organization_id: organization.id})
+    {user, organization, source}
+  end
+
+  def fixture(:event_schema, source) do
+    {:ok, event_schema} = Events.create_event_schema(%{@create_attrs | source_id: source.id})
     event_schema
   end
 
   setup %{conn: conn} do
-    {:ok, user} = EventValidator.Accounts.create_user(@user_attrs)
+    {user, _organization, source} = setup_fixture()
     token = JWT.encode_token(user, %{})
 
     {:ok,
      conn:
        conn
        |> put_req_header("accept", "application/json")
-       |> put_req_header("authorization", "bearer: " <> token)}
+       |> put_req_header("authorization", "bearer: " <> token),
+     source: source}
   end
 
   describe "index" do
@@ -45,8 +66,12 @@ defmodule EventValidatorWeb.EventSchemaControllerTest do
   end
 
   describe "create event_schema" do
-    test "renders event_schema when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.event_schema_path(conn, :create), event_schema: @create_attrs)
+    test "renders event_schema when data is valid", %{conn: conn, source: source} do
+      conn =
+        post(conn, Routes.event_schema_path(conn, :create),
+          event_schema: %{@create_attrs | source_id: source.id}
+        )
+
       assert %{"id" => id} = json_response(conn, 201)["data"]
 
       conn = get(conn, Routes.event_schema_path(conn, :show, id))
@@ -65,12 +90,12 @@ defmodule EventValidatorWeb.EventSchemaControllerTest do
   end
 
   describe "update event_schema" do
-    setup [:create_event_schema]
-
     test "renders event_schema when data is valid", %{
       conn: conn,
-      event_schema: %EventSchema{id: id} = event_schema
+      source: source
     } do
+      %EventSchema{id: id} = event_schema = fixture(:event_schema, source)
+
       conn =
         put(conn, Routes.event_schema_path(conn, :update, event_schema),
           event_schema: @update_attrs
@@ -87,7 +112,9 @@ defmodule EventValidatorWeb.EventSchemaControllerTest do
              } = json_response(conn, 200)["data"]
     end
 
-    test "renders errors when data is invalid", %{conn: conn, event_schema: event_schema} do
+    test "renders errors when data is invalid", %{conn: conn, source: source} do
+      event_schema = fixture(:event_schema, source)
+
       conn =
         put(conn, Routes.event_schema_path(conn, :update, event_schema),
           event_schema: @invalid_attrs
@@ -98,9 +125,8 @@ defmodule EventValidatorWeb.EventSchemaControllerTest do
   end
 
   describe "delete event_schema" do
-    setup [:create_event_schema]
-
-    test "deletes chosen event_schema", %{conn: conn, event_schema: event_schema} do
+    test "deletes chosen event_schema", %{conn: conn, source: source} do
+      event_schema = fixture(:event_schema, source)
       conn = delete(conn, Routes.event_schema_path(conn, :delete, event_schema))
       assert response(conn, 204)
 
@@ -133,10 +159,5 @@ defmodule EventValidatorWeb.EventSchemaControllerTest do
 
       assert json_response(conn, 400)["errors"] == %{"title" => "BadRequest"}
     end
-  end
-
-  defp create_event_schema(_) do
-    event_schema = fixture(:event_schema)
-    {:ok, event_schema: event_schema}
   end
 end
