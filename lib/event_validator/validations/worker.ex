1df57fab1b0ae18728d302f1376a3dfe9464bd4f
Luiz Parreira <luiz@magnetis.com.br>
2019-10-30 08:04:13 -0300

    Remove `"type" => "track"` from validation pattern matching
    - This is being done so that we only filter events to be validated based
    on if the event has a payload of `event`, where the event name is
    supposed to be codified

--- lib/event_validator/validations/worker.ex
+++ lib/event_validator/validations/worker.ex
@@ -2,7 +2,7 @@ defmodule EventValidator.Validations.Worker do
   alias EventValidator.{Events, Validations}
   alias Validations.ValidateSchema
 
-  def perform(source_id, %{"event" => event_name, "type" => "track"} = params) do
+  def perform(source_id, %{"event" => event_name} = params) do
     case Events.get_event_schema_by(%{source_id: source_id, name: event_name}) do
       nil -> Events.infer_event_schema(params, source_id)
       event_schema -> ValidateSchema.validate(event_schema, params)
